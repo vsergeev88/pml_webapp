666f2f07eaa563887bea8822e145325424e91bbc
Valentin Sergeev <valentin@visme.com>
2024-12-03 20:35:57 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs'
-  },
-  base: './'
+  // build: {
+  //   outDir: './docs'
+  // },
+  // base: './'
 })
