049ce19355339fb1540327c93cc861ce6ca97a1d
Valentin Sergeev <valentin@visme.com>
2024-12-03 19:24:16 +0300

    add tg

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const tg = window.Telegram?.WebApp
+
   return (
     <>
       <div>
@@ -27,7 +29,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {JSON.stringify(tg)}
       </p>
     </>
   )
